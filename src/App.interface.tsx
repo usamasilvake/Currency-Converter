7f90ec098bafb9aa5f770ccf80b3e1f384211e72
Muhammad Osama <usamasilvake@gmail.com>
2022-06-02 18:27:27 +0500

    feat: Add typescript

---
+++ src/App.interface.tsx
@@ -0,0 +1,12 @@
+export interface CurrencyInterface{
+    [key:string]: CurrencyUSDInterface;
+}
+
+
+export interface CurrencyUSDInterface {
+    [key: string]: number
+}
+
+export interface CurrencyInfoInterface{
+    [key: string]: number ;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
-// import { HiSwitchHorizontal } from 'react-icons/hi';
 import './App.scss';
 import Dropdown from './dropdown/Dropdown';
-const App = () => {
+import { CurrencyInfoInterface, CurrencyInterface } from './App.interface';
 
-	// Initializing all the state variables
-	const [info, setInfo] = useState<{[key: string]: number}>({});
+const App = () => {
+	const [info, setInfo] = useState<CurrencyInfoInterface>({});
 	const [input, setInput] = useState(100);
 	const [from, setFrom] = useState("usd");
 	const [to, setTo] = useState("pkr");
 	const [options, setOptions] = useState<string[]>([]);
-	const [output, setOutput] = useState<number>(199);
+	const [output, setOutput] = useState<number>(198);
 
-	// Calling the api whenever the dependency changes
 	useEffect(() => {
-		Axios.get(
-			`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
-			.then((res:any) => {
-				setInfo(res.data[from]);
-			})
+		const fetchCurrencies = () => {
+			const get = async <T,>(url: string): Promise<T> => {
+				const api = await Axios.get(url);
+				return api.data as T;
+			}
+			get<CurrencyInterface>(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`).then
+				((res) => {
+					setInfo(res[from]);
+					console.log(res)
+				})
+		}
+		fetchCurrencies();
 	}, [from]);
 
 	useEffect(() => {
@@ -27,32 +32,33 @@ const App = () => {
 		setOutput(input * info[to]);
 	}, [info, input, to])
 
+	const flip = () => {
+		var temp = from;
+		setFrom(to);
+		setTo(temp)
+	}
 
-	// const flip = () => {
-	// 	var temp = from;
-	// 	setFrom(to);
-		// setTo(temp)
-	// }
 	return (
 		<div className='cu-converter'>
 			<div className="cu-app">
 				<div className="cu-heading">
 					<h1>Currency converter</h1>
 				</div>
-				<div className="currency">
+				<div className="cu-currency">
 					<div className="cu-amount">
 						<h3>Amount</h3>
 						<input type="number" min={0}
 							placeholder="Enter the amount" value={input}
-							onChange={(e:any) => setInput(e.target.value)} />
+							onChange={(e: any) => setInput(e.target.value)} />
 					</div>
 					<div className="cu-options">
 						<div className="cu-option">
 							<h3>From</h3>
 							<Dropdown options={options}
 								onChange={(e) => { setFrom(e) }}
-								initial={from}  />
+								initial={from} />
 						</div>
+						<div onClick={flip}>flip</div>
 						<div className="cu-option">
 							<h3>To</h3>
 							<Dropdown options={options}
